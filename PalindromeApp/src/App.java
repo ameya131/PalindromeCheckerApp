ca06294b990e7acd6c1ea7aa6cfbdd6f9a4d997a
ameya131 <guptas.ameya@gmail.com>
2026-02-17 16:00:28 +0530

    [AMEYA]HARDCODED

--- PalindromeApp/src/App.java
+++ PalindromeApp/src/App.java
@@ -1,46 +1,26 @@
-/**
- * =========================================================
- * MAIN CLASS - UseCase1PalindromeCheckerApp
- * =========================================================
- *
- * Use Case 1: Application Entry & Welcome Message
- *
- * Description:
- * This class represents the entry point of the
- * Palindrome Checker Management System.
- *
- * At this stage, the application:
- * - Starts execution from the main() method
- * - Displays a welcome message
- * - Shows application version
- *
- * No palindrome logic is implemented yet.
- *
- * The goal is to establish a clear startup flow.
- *
- * @author user1
- * @version 1.0
- */
-
 public class App {
 
     /**
-     * Application entry point.
-     *
-     * This is the first method executed by the JVM
-     * when the program starts.
-     *
+     * Application entry point for UC2.
      * @param args Command-line arguments
      */
     public static void main(String[] args) {
 
-        // Display Welcome Message
-        System.out.println("Welcome to the Palindrome Checker Management System");
+        // Hardcoded string
+        String input = "madam";
 
-        // Display Application Version
-        System.out.println("Version : 1.0");
+        boolean isPalindrome = true;
 
-        // Display Initialization Message
-        System.out.println("System initialized successfully.");
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        System.out.println("Input text: " + input);
+        System.out.println("Is it a Palindrome? : " + isPalindrome);
     }
 }
